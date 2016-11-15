6b3394ed072108f17dac7bcd306310ad964c1024
chensheng <c4w@mp.local>
2016-11-15 10:37:17 +0800

    /p /n 下的图片二次处理,移动到同级目录

--- imports/api/fp.js
+++ imports/api/fp.js
@@ -9,17 +9,25 @@ class CFolder {
   constructor() {
   }
   pass(pics, bPass){
-    console.log(pics+':'+bPass);
     pics.forEach(function(item){
       let fsrc = settings.pic_root+g_path.sep+item;
       let pos = fsrc.lastIndexOf(g_path.sep);
       let fpath = fsrc.substring(0,pos);
+      let ftarget=null;
       let fsub = bPass? 'p':'n';
-      let ftarget = fsrc.substring(0,pos+1)+fsub+fsrc.substring(pos);
+      //已经通过或删除的文件需要特殊处理
+      console.log('fpath:'+fpath);
+
+      if(fpath.endsWith('/p') || fpath.endsWith('/n')){
+          ftarget = fsrc.substring(0,pos-1)+fsub+fsrc.substring(pos);
+      }else{
+          ftarget = fsrc.substring(0,pos+1)+fsub+fsrc.substring(pos);
+      }
       console.log('src:'+fsrc+ '\ntarget:'+ftarget);
       mv(fsrc, ftarget,{mkdirp: true}, function(err) {
           // handle the error
-          console.log(err);
+          if(err)
+            console.log(err);
       });
     });
 
@@ -28,7 +36,8 @@ class CFolder {
     let fn = settings.pic_root+ fpath+ g_path.sep+ uuid.v1()+".jpg";
     let base64Data = data.replace(/^data:image\/jpeg;base64,/, "");
     g_fs.writeFile(fn, new Buffer(base64Data, 'base64'), function(err) {
-      console.log(err);
+      if(err)
+        console.log(err);
     });
   }
   getPics(fpath){
