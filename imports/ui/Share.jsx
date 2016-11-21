bd2fa941b4612fe9580856a571922397d5a65b7d
chensheng <c4w@mp.local>
2016-11-21 18:11:56 +0800

    share瀑布流显示

--- imports/ui/Share.jsx
+++ imports/ui/Share.jsx
@@ -1,97 +1,57 @@
-import React, { Component, PropTypes } from 'react';
-import {Responsive, WidthProvider} from 'react-grid-layout';
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-const ReactGridLayout = require('react-grid-layout');
-// Paint component - represents a single todo item
+import React, { Component, PropTypes } from 'react';
+const Masonry = require('react-masonry-component');
+
+const settings = require('../../settings.js');
+
+const masonryOptions = {
+    transitionDuration: 0
+};
 export default class Share extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      layout: [],
-      currentBreakpoint: 'lg',
-      mounted: false,
-      layouts: {lg: this.props.initialLayout},
-    };
-
-  }
-
-  componentDidMount() {
-    this.setState({mounted: true});
-  }
-
-  generateDOM() {
-    return _.map(this.state.layouts.lg, function (l, i) {
-      return (
-        <div key={i} className={l.static ? 'static' : ''}>
-          {l.static ?
-            <span className="text" title="This item is static and cannot be removed or resized.">Static - {i}</span>
-            : <span className="text">{i}</span>
-          }
-        </div>);
-    });
-  }
-
-  onBreakpointChange(breakpoint) {
-    this.setState({
-      currentBreakpoint: breakpoint
-    });
-  }
-
-  onLayoutChange(layout, layouts) {
-    //this.props.onLayoutChange(layout, layouts);
-    this.setState({layout: layout});
-  }
-
-  onNewLayout() {
-    this.setState({
-      layouts: {lg: generateLayout()}
+    let me = this;
+    this.state = {path:'upload',pics:[]};
+    Meteor.call('folder.getpics', this.state.path, function(error, result){
+        if(error){
+            console.log(error);
+        } else {
+            //console.log('----正在请求图片：'+result);
+            me.setState({pics:result,sb_open:true,
+              sb_msg:'当前目录共'+result.length+'张图'});
+            //console.log('--请求结束--'+result);
+        }
     });
   }
 
   render() {
+    console.log('render');
+    let w=window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth;
+    w -= 20;
+    //屏幕宽度小于抽点图片尺寸,则强制图片与屏幕等宽
+    let img_w = w < 450 ? w: 450;
+    let path_imgsrc = settings.url_root+ this.state.path.replace(/\\/g,'/')  +'/';
+    if(settings.thumbnails_size>0)
+      path_imgsrc+= settings.thumbnails_uri+settings.thumbnails_size +'/';
+    
+    var childElements = this.state.pics.map(function(pic,index){
+        return (
+          <div className="effect2" key={index} >
+                <img width={img_w} src={path_imgsrc+pic.fn} />
+          </div>
+        );
+    });
+
     return (
-      <div>
-        <div>Current Breakpoint: {this.state.currentBreakpoint} ({this.props.cols[this.state.currentBreakpoint]} columns)
-        </div>
-        <button onClick={this.onNewLayout.bind(this)}>Generate New Layout</button>
-        <ResponsiveReactGridLayout
-          {...this.props}
-          layouts={this.state.layouts}
-          onBreakpointChange={this.onBreakpointChange.bind(this)}
-          onLayoutChange={this.onLayoutChange.bind(this)}
-          // WidthProvider option
-          measureBeforeMount={false}
-          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
-          // and set `measureBeforeMount={true}`.
-          useCSSTransforms={this.state.mounted}>
-          {this.generateDOM()}
-        </ResponsiveReactGridLayout>
-      </div>
+        <Masonry
+            className={''} // default ''
+            elementType={'div'} // default 'div'
+            options={masonryOptions} // default {}
+            disableImagesLoaded={false} // default false
+            updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+        >
+            {childElements}
+        </Masonry>
     );
   }
 }
-
-Share.defaultProps = {
-  className: "layout",
-  rowHeight: 30,
-  cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs: 2},
-  initialLayout: generateLayout()
-};
-
-function generateLayout() {
-  return _.map(_.range(0, 25), function (item, i) {
-    var y = Math.ceil(Math.random() * 4) + 1;
-    return {
-      x: _.random(0, 5) * 2 % 12,
-      y: Math.floor(i / 6) * y,
-      w: 2,
-      h: y,
-      i: i.toString(),
-      //static: Math.random() < 0.05
-      static: true
-    };
-  });
-}
-
